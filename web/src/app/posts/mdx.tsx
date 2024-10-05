d8e7d933fe802fe88c2c9074ee2145b94ca48279
tiendvlp <tiendvlp@gmail.com>
2024-10-05 20:13:18 +0700

    Add lottie

--- web/src/app/posts/mdx.tsx
+++ web/src/app/posts/mdx.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useRef, useCallback } from 'react'
 import {useMDXComponent} from 'next-contentlayer/hooks'
+import { DotLottie } from '@lottiefiles/dotlottie-web'
 
 function CustomImage(props: any) {
   const src = props.src.includes('http')
@@ -24,9 +25,43 @@ function L2(props: any) {
   </div> 
 }
 
+function Lottie(props: any) {
+  const {
+    className = '',
+    file
+  } = props || {}
+
+  const canvasRef = useRef<HtmlElement | undefined>()
+
+  const lottieFile = useRef<any>()
+
+  if (!file) throw new Error('Lottie file must be defined')
+
+  useEffect(() => {
+    if (!file || !canvasRef.current) return
+
+    lottieFile.current = new DotLottie({
+      autoPlay: true,
+      loop: true,
+      canvas: canvasRef.current!,
+      src: file
+    })
+  }, [canvasRef.current, file])
+
+  const onPlayClick = useCallback(() => {
+    lottieFile.current?.play()
+  }, [lottieFile.current])
+
+  return <>
+    <div className={`flex flex-col w-full h-full ${className}`}>
+      <canvas ref={canvasRef} className='w-full h-full'/>
+      <button onClick={onPlayClick}>Play</button>
+    </div>
+  </>
+}
+
 function _MdxContent (props: any) {
   const {code} = props || {}
-  console.log(code)
   const MDXContent = useMDXComponent(code)
 
   return <div className='
@@ -56,10 +91,12 @@ function _MdxContent (props: any) {
 const mdxComponents = {
   img: (props: any) => <CustomImage {...props}></CustomImage>,
   L2: (props: any) => <L2 {...props}/>,
-  Mdx: (props: any) => <_MdxContent code={props.children}/>
+  Mdx: (props: any) => <_MdxContent code={props.children}/>,
+  Lottie: (props:any) => <Lottie {...props}/>
 }
 
 
 export const MdxContent = (props: any) => {
   return <_MdxContent code={props.post.body.code}/>
 }
+
